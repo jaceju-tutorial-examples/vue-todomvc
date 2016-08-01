c740da9e09e3af1e9a96ef6e1d8f2e19d43ab3fc
Jace Ju <jaceju@gmail.com>
2016-08-01 14:00:21 +0800

    撰寫程式以通過：建立新待辦事項後應該出現待辦事項列表
    1. data 中新增 todos 陣列。
    2. 利用 v-for 來建立列表。
    3. 在 addTodo 中修改 todos 。

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,34 @@
              v-model="newTodo"
              @keyup.enter="addTodo">
     </header>
+    <section class="main">
+      <ul class="todo-list">
+        <li class="todo"
+            v-for="todo in todos">
+          <div class="view">
+            <label>{{ todo.title }}</label>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 <style src="todomvc-app-css/index.css"></style>
 <script>
   export default {
     data () {
-      return { newTodo: '' };
+      return {
+        newTodo: '',
+        todos: []
+      };
     },
     methods: {
       addTodo () {
+        const todo = this.newTodo && this.newTodo.trim();
+        if (!todo) {
+          return;
+        }
+        this.todos.push({ title: todo, completed: false });
         this.newTodo = '';
       }
     }
