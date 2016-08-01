a1cbb51d611bea4c935e4e271b748a7253153807
Jace Ju <jaceju@gmail.com>
2016-08-01 17:46:57 +0800

    撰寫程式以通過： Store 應可儲存資料到 localStorage 中

--- src/store.js
+++ src/store.js
@@ -3,5 +3,8 @@ const STORAGE_KEY = 'vue-todomvc';
 module.exports = {
   fetch: function () {
     return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 };
