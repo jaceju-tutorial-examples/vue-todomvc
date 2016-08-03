b317054b18007d4e5195c2746f3db506c419f725
Jace Ju <jaceju@kkbox.com>
2016-08-03 12:28:57 +0800

    撰寫程式以通過：輸入欄位應該在輸入待辦事項後清空
    1. 用 v-model 來綁 input 輸入值。
    2. 用 @keyup.enter 來綁事件處理函式。

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,22 @@
       <h1>todos</h1>
       <input class="new-todo"
              autofocus autocomplete="off"
-             placeholder="What needs to be done?">
+             placeholder="What needs to be done?"
+             v-model="newTodo"
+             @keyup.enter="addTodo">
     </header>
   </div>
 </template>
 <style src="todomvc-app-css/index.css"></style>
 <script>
-  export default {};
+  export default {
+    data () {
+      return { newTodo: '' };
+    },
+    methods: {
+      addTodo () {
+        this.newTodo = '';
+      }
+    }
+  };
 </script>
